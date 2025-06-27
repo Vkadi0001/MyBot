6b759de68151155bb2b79c5b89337de0f25dd245
Vkadi0001 <vladi0001@yandex.ru>
2025-06-27 22:30:09 +0300

    Add files via upload

---
+++ config.py
@@ -0,0 +1,3 @@
+BOT_TOKEN = "8180544323:AAGU6E71QyjwCtmNZjg0amdUNjtjo0Dhn00"  # От @BotFather
+YC_API_KEY = "AQVN....y0__xCJsOQUGMHdEyDAxaXVEzBK3JTKoopKV_09Y3j6pVAvhb2B.."  # Ключ Yandex Cloud
+YC_FOLDER_ID = "b1gs2ngg395dun9vmn2d"  # ID каталога
